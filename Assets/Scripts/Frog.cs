ed7443d4400fe10175105e1b3e0851a7919619e6
Diogo Rodrigues <diogofgr@gmail.com>
2017-06-19 21:16:00 +0100

    start repo

---
+++ Assets/Scripts/Frog.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Frog : MonoBehaviour {
+
+	private Vector3 jumpVector = new Vector3(0,1,1);
+
+	void Start() {
+		
+	}
+
+	void FixedUpdate() {
+		Debug.DrawRay (transform.position, jumpVector, Color.blue);
+
+		var projectedJumpVector = Vector3.ProjectOnPlane (jumpVector, Vector3.up);
+
+		var radiansToRotate = Mathf.Deg2Rad * 45;
+		var rotatedJumpVector = Vector3.RotateTowards(projectedJumpVector, Vector3.up, radiansToRotate, 0);
+
+		Debug.DrawRay (transform.position, projectedJumpVector, Color.blue);
+		Debug.DrawRay (transform.position, rotatedJumpVector.normalized, Color.red);
+
+		if (Input.GetMouseButtonDown(0)){
+			GetComponent<Rigidbody>().AddForce (jumpVector, ForceMode.Impulse);
+		}
+	}
+
+}
---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+	static MusicPlayer instance = null;
+
+	void Awake() {
+		if (instance != null) {
+			// destroy new music player (keep previous):
+//			Destroy (gameObject);
+			Debug.Log ("Kept previous music player");
+		}
+		else {
+			// create music player:			
+			instance = this;
+			GameObject.DontDestroyOnLoad (gameObject);
+			Debug.Log ("Created music player");
+			Debug.Log (instance);
+			Debug.Log (gameObject);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
